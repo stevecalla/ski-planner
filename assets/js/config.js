e8c309d1d2040905f050d6a3cb6684f1d03ad0c3
Dan Kelly <dpk5e7@yahoo.com>
2022-07-29 20:52:39 -0600

    Got directions to work with MapQuest API.  Added Bulma so that I could use the is-invisible class.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
 const config = {
   OPEN_WEATHER_KEY: "739b5591a050cea4655cf79b5b0fc62b",
   GOOGLE_MAPS_KEY: "AIzaSyBrPbBvNhBCNiSVv3-BJl-n95Hymqd0RPo",
+  MAPQUEST_KEY: "onM30fdvaziP9ykjaYeleR5hvIhOmLm1",
 };
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,12 @@
 let searchForm = document.querySelector("#searchForm");
 let ddPass = document.querySelector("#ddPass");
 let drivingDirections = document.querySelector("#drivingDirections");
+let txtStartAddress = document.querySelector("#txtStartAddress");
+let txtStartDate = document.querySelector("#txtStartDate");
+let txtStartTime = document.querySelector("#txtStartTime");
+let tblDirections = document.querySelector("#tblDirections");
+let tdTotalDistance = document.querySelector("#tdTotalDistance");
+let tdTotalTime = document.querySelector("#tdTotalTime");
 let weatherForecast = document.querySelector("#weatherForecast");
 let snowConditions = document.querySelector("#snowConditions");
 
@@ -30,16 +36,146 @@ function getResortInfo(event) {
     (element) => element.Name === event.target.dataset.skiarea
   );
 
-  alert(
-    `${skiArea.Name}\n${skiArea.Pass}\n${skiArea.Latitude}\n${skiArea.Longitude}`
-  );
-
   displayDrivingDirections(skiArea);
   displayWeatherForecast(skiArea);
   displaySnowConditions(skiArea);
+
+  drivingDirections.classList.remove("is-invisible");
+  weatherForecast.classList.remove("is-invisible");
+  snowConditions.classList.remove("is-invisible");
 }
 
-function displayDrivingDirections(skiArea) {}
+async function displayDrivingDirections(skiArea) {
+  startCoordinates = txtStartAddress.value;
+  if (!isLatLong(startCoordinates)) {
+    startCoordinates = await fetchCoordinatesFromAddress(txtStartAddress.value);
+  }
+
+  let endCoordinates = `${skiArea.Latitude},${skiArea.Longitude}`;
+  let directions = await fetchDirections(startCoordinates, endCoordinates);
+
+  removeAllChildNodes(tblDirections);
+
+  // Last element is the totals
+  for (let i = 0; i < directions.length - 1; i++) {
+    let tr = document.createElement("tr");
+
+    // Step #
+    let td = document.createElement("td");
+    td.textContent = i + 1;
+    tr.appendChild(td);
+
+    // Narrative
+    td = document.createElement("td");
+    td.textContent = directions[i].narrative;
+    tr.appendChild(td);
+
+    // Distance
+    td = document.createElement("td");
+    td.textContent = `${directions[i].distance.toFixed(1)} miles`;
+    tr.appendChild(td);
+
+    // Latitude
+    // td = document.createElement("td");
+    // td.textContent = directions[i].latitude;
+    // tr.appendChild(td);
+
+    // Longitude
+    // td = document.createElement("td");
+    // td.textContent = directions[i].longitude;
+    // tr.appendChild(td);
+
+    // Time
+    // td = document.createElement("td");
+    // td.textContent = directions[i].time;
+    // tr.appendChild(td);
+
+    // formattedTime
+    td = document.createElement("td");
+    td.textContent = directions[i].formattedTime;
+    tr.appendChild(td);
+
+    // Map
+    if (directions[i].mapUrl) {
+      td = document.createElement("td");
+      let img = document.createElement("img");
+      img.src = directions[i].mapUrl;
+      img.alt = "Map";
+      td.appendChild(img);
+      tr.appendChild(td);
+    }
+
+    tblDirections.appendChild(tr);
+  }
+
+  tdTotalDistance.textContent = `${directions[
+    directions.length - 1
+  ].distance.toFixed(1)} miles`;
+  tdTotalTime.textContent = directions[directions.length - 1].formattedTime; // time is in seconds
+}
+
+async function fetchDirections(startCoordinates, endCoordinates) {
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Cookie",
+    "JSESSIONID=F03321F222C277ED923D3B8EF447698D; JSESSIONID=885FD8B936A29FF149EE75F9D0C469D8"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  //http://www.mapquestapi.com/directions/v2/route?key=onM30fdvaziP9ykjaYeleR5hvIhOmLm1&from=39.633077,-105.1388028&to=39.68,-105.897
+  let apiUrl = `http://www.mapquestapi.com/directions/v2/route?key=${config.MAPQUEST_KEY}&from=${startCoordinates}&to=${endCoordinates}`;
+
+  const response = await fetch(apiUrl, requestOptions);
+  const data = await response.json();
+
+  const maneuvers = data.route.legs[0].maneuvers;
+  const directions = [];
+  for (let i = 0; i < maneuvers.length; i++) {
+    let step = {
+      distance: maneuvers[i].distance,
+      narrative: maneuvers[i].narrative,
+      latitude: maneuvers[i].startPoint.lat,
+      longitude: maneuvers[i].startPoint.lng,
+      time: maneuvers[i].time,
+      formattedTime: maneuvers[i].formattedTime,
+      mapUrl: maneuvers[i].mapUrl,
+    };
+    directions.push(step);
+  }
+
+  let totals = {
+    distance: data.route.distance,
+    time: data.route.realTime,
+    formattedTime: data.route.formattedTime,
+  };
+  directions.push(totals);
+
+  return directions;
+}
+
+async function fetchCoordinatesFromAddress(address) {
+  var myHeaders = new Headers();
+  myHeaders.append("Cookie", "JSESSIONID=F2079DB8A9017A9DEF9E1957C80C1A06");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  let apiUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${config.MAPQUEST_KEY}&location=${address}`;
+
+  const response = await fetch(apiUrl, requestOptions);
+  const data = await response.json();
+
+  const coord = data.results[0].locations[0].latLng;
+  return `${coord.lat},${coord.lng}`;
+}
 
 function displayWeatherForecast(skiArea) {}
 
@@ -111,8 +247,32 @@ ddPass.addEventListener("change", function () {
   displayMarkers(ddPass.value);
 });
 
+function getDefaultStartValue() {
+  if (localStorage.getItem("StartAddress")) {
+    return localStorage.getItem("StartAddress");
+  } else {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      txtStartAddress.value = `${pos.coords.latitude},${pos.coords.longitude}`;
+    }
+
+    function error(err) {}
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+}
+
 function init() {
   initMap();
   displayMarkers("");
+
+  // Load default values for Directions Start Controls
+  txtStartDate.value = moment().add(1, "day").format("yyyy-MM-DD");
+  getDefaultStartValue();
 }
 init();
