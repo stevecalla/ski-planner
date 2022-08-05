107894318e8bde114cc0ec56ac5b78ce3fdcf523
Dan Kelly <97580207+dpk5e7@users.noreply.github.com>
2022-08-05 09:03:05 -0600

    Added checkboxes to map page and removed dropdownlist

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 //section:query selector variables go here 👇
-let searchForm = document.querySelector("#searchForm");
-let ddPass = document.querySelector("#ddPass");
+let chkEpic = document.querySelector("#chkEpic");
+let chkIkon = document.querySelector("#chkIkon");
+let chkIndependent = document.querySelector("#chkIndependent");
 let modalProfileFromHomePage = document.getElementById(
   "modal-profile-homepage-button"
 );
@@ -34,9 +35,6 @@ function displayMarkers(pass) {
       return skiArea.pass.trim().toLowerCase() === pass.trim().toLowerCase();
     });
   }
-
-  // remove all the markers
-  markers.clearLayers();
 
   const ResortIcon = L.Icon.extend({
     options: {
@@ -83,31 +81,11 @@ function displayMarkers(pass) {
     marker.bindTooltip(`<h4>${filteredSkiAreas[i].name}</h4>`, toolTipOptions);
 
     function markerClick(event) {
-      // http://api.powderlin.es/closest_stations
-      // https://dqmoczhn0pnkc.cloudfront.net/closest_stations
-
       const skiArea = skiAreas.find(
         (element) =>
           element.latitude === event.latlng.lat &&
           element.longitude === event.latlng.lng
       );
-
-      var settings = {
-        url: "https://dqmoczhn0pnkc.cloudfront.net/closest_stations",
-        method: "GET",
-        timeout: 0,
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
-          lat: skiArea.latitude,
-          lng: skiArea.longitude,
-          data: true,
-          days: 0,
-          count: 3,
-        },
-      };
-
-      let dataSNOTEL;
 
       // Initial popup text
       let popupText = `<h4>${skiArea.name}</h4>
@@ -129,46 +107,84 @@ function displayMarkers(pass) {
         .setContent(popupText)
         .openOn(map);
 
-      $.ajax(settings)
-        .done(function (response) {
-          let i = 0;
-          do {
-            dataSNOTEL = response[i++];
-            if (dataSNOTEL.data.length > 0) {
-              // Data is:
-              // `Closest SNOTEL: ${dataSNOTEL.station_information.name}`
-              // `Elevation: ${dataSNOTEL.station_information.elevation} ft`
-              // `Distance Away: ${dataSNOTEL.distance.toFixed(2)} miles`
-              // `Date: ${dataSNOTEL.data[0]["Date"]}`
-              // `Snow Water Equivalent (in): ${dataSNOTEL.data[0]["Snow Water Equivalent (in)"]}`
-              // `Change In Snow Water Equivalent (in): ${dataSNOTEL.data[0]["Change In Snow Water Equivalent (in)"]}`
-              // `Snow Depth (in): ${dataSNOTEL.data[0]["Snow Depth (in)"]}`
-              // `Change In Snow Depth (in): ${dataSNOTEL.data[0]["Change In Snow Depth (in)"]}`
-              // `Observed Air Temperature (degrees farenheit): ${dataSNOTEL.data[0]["Observed Air Temperature (degrees farenheit)"]}`
+      displaySNOTELDataInPopup(skiArea, popup);
+    }
+    marker.addEventListener("click", markerClick);
+  }
+}
 
-              // Update the popup text with SNOTEL data
-              popupText = `<h4>${skiArea.name}</h4>
+function displaySNOTELDataInPopup(skiArea, popup) {
+  // http://api.powderlin.es/closest_stations
+  // https://dqmoczhn0pnkc.cloudfront.net/closest_stations
+
+  var settings = {
+    url: "https://dqmoczhn0pnkc.cloudfront.net/closest_stations",
+    method: "GET",
+    timeout: 0,
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+      lat: skiArea.latitude,
+      lng: skiArea.longitude,
+      data: true,
+      days: 0,
+      count: 3,
+    },
+  };
+
+  let dataSNOTEL;
+
+  $.ajax(settings)
+    .done(function (response) {
+      let i = 0;
+      do {
+        dataSNOTEL = response[i++];
+        if (dataSNOTEL.data.length > 0) {
+          // Data is:
+          // `Closest SNOTEL: ${dataSNOTEL.station_information.name}`
+          // `Elevation: ${dataSNOTEL.station_information.elevation} ft`
+          // `Distance Away: ${dataSNOTEL.distance.toFixed(2)} miles`
+          // `Date: ${dataSNOTEL.data[0]["Date"]}`
+          // `Snow Water Equivalent (in): ${dataSNOTEL.data[0]["Snow Water Equivalent (in)"]}`
+          // `Change In Snow Water Equivalent (in): ${dataSNOTEL.data[0]["Change In Snow Water Equivalent (in)"]}`
+          // `Snow Depth (in): ${dataSNOTEL.data[0]["Snow Depth (in)"]}`
+          // `Change In Snow Depth (in): ${dataSNOTEL.data[0]["Change In Snow Depth (in)"]}`
+          // `Observed Air Temperature (degrees farenheit): ${dataSNOTEL.data[0]["Observed Air Temperature (degrees farenheit)"]}`
+
+          // Update the popup text with SNOTEL data
+          popupText = `<h4>${skiArea.name}</h4>
                 <p>Snow Depth (in): ${dataSNOTEL.data[0]["Snow Depth (in)"]}<br />
                 Change In Snow Depth (in): ${dataSNOTEL.data[0]["Change In Snow Depth (in)"]}<br />
                 Air Temperature: ${dataSNOTEL.data[0]["Observed Air Temperature (degrees farenheit)"]} \xB0F</p>
                 <a class="button custom-button is-small is-info is-light" href="./dashboard.html?resort=${skiArea.name}">Details ➡️</a>
               `;
 
-              popup.setContent(popupText);
-            }
-            // If the closest SNOTEL station does not have any data, go to the next one.  Powderhorn is this way.  Pulling 3 stations just in case.
-          } while (dataSNOTEL.data.length === 0);
-        })
-        .fail()
-        .always();
-    }
-    marker.addEventListener("click", markerClick);
-  }
+          popup.setContent(popupText);
+        }
+        // If the closest SNOTEL station does not have any data, go to the next one.  Powderhorn is this way.  Pulling 3 stations just in case.
+      } while (dataSNOTEL.data.length === 0);
+    })
+    .fail()
+    .always();
 }
 
-ddPass.addEventListener("change", function () {
-  displayMarkers(ddPass.value);
-});
+function passChecked(event) {
+  // remove all the markers
+  markers.clearLayers();
+
+  if (chkEpic.checked) {
+    displayMarkers(chkEpic.value);
+  }
+  if (chkIkon.checked) {
+    displayMarkers(chkIkon.value);
+  }
+  if (chkIndependent.checked) {
+    displayMarkers(chkIndependent.value);
+  }
+}
+chkEpic.addEventListener("click", passChecked);
+chkIkon.addEventListener("click", passChecked);
+chkIndependent.addEventListener("click", passChecked);
 
 function init() {
   try {
